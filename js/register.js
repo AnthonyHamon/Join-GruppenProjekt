c61fbcb4ba54cef24969cccf2cfa2c79cb5b47aa
Anthony Hamon <anthony.hamon91@gmail.com>
2023-09-23 19:06:28 +0200

    sign up page finished

--- js/register.js
+++ js/register.js
@@ -1,5 +1,9 @@
 function toggleRememberMeButton() {
     document.getElementById('check-button').classList.toggle('d-none');
     document.getElementById('checked-button').classList.toggle('d-none');
+}
 
+function checkPrivatePolicyButton() {
+    document.getElementById('check-button').classList.toggle('d-none');
+    document.getElementById('checked-button').classList.toggle('d-none');
 }
