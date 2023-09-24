52d29835eaacb8418690790187dcb1fe3cb5b816
Anthony Hamon <anthony.hamon91@gmail.com>
2023-09-24 10:32:42 +0200

    update login images

--- js/register.js
+++ js/register.js
@@ -7,3 +7,31 @@ function checkPrivatePolicyButton() {
     document.getElementById('check-button').classList.toggle('d-none');
     document.getElementById('checked-button').classList.toggle('d-none');
 }
+
+function togglePasswordImg() {
+    let passwordVisibility = document.getElementById('password-visibility-on');
+    let passwordVisibilityOff = document.getElementById('password-visibility-off');
+    let passwordLock = document.getElementById('password-lock');
+    let passwordInput = document.getElementById('password-input');
+    if (!passwordInput.value == 0) {
+        passwordLock.classList.add('d-none');
+        passwordVisibilityOff.classList.remove('d-none');
+    } else {
+        passwordLock.classList.remove('d-none');
+        passwordVisibilityOff.classList.add('d-none');
+        passwordVisibility.classList.add('d-none');
+
+    }
+}
+
+function toggleShowPassword() {
+    let passwordInput = document.getElementById('password-input');
+    let passwordVisibilityOff = document.getElementById('password-visibility-off');
+    let passwordVisibility = document.getElementById('password-visibility-on');
+    passwordVisibilityOff.classList.toggle('d-none');
+    passwordVisibility.classList.toggle('d-none');
+    if (!passwordInput.value == 0) {
+        passwordInput.type.toggle = "text";
+    }
+
+}
