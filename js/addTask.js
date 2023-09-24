b6ba733c625e4aebe43d1f2cfc03b4a67b95d2da
Anthony Hamon <anthony.hamon91@gmail.com>
2023-09-24 21:06:45 +0200

    upload to be up to date

--- js/addTask.js
+++ js/addTask.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-=======
 function changeCurrentTemplate() {
     showCurrentBgrColor();
     removeOtherBgrColor();
@@ -32,5 +18,4 @@ function renderHTML() {
     content.innerHTML = /*html*/`
         <h1>Add Task</h1>
     `;
->>>>>>> 631436dde392137848e69fe5cd7b5300322d38d2
 }
