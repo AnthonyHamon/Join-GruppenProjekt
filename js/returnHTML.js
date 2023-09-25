bdc6e4cf34f25ec2835b2eb00dfbd0056e3cfa0c
René Porzelt <rene.tube992@gmail.com>
2023-09-25 09:25:15 +0200

    added provisionally title in all templates

--- js/returnHTML.js
+++ js/returnHTML.js
@@ -1,19 +1,37 @@
+let content = document.getElementById('content');
+
 function generateSummaryHTML() {
+    content.innerHTML = '';
+    content.innerHTML =/*html*/`
+        <h1>Summary</h1>
+    `;
     console.clear();
     console.table({ name: 'generateSummaryHTML()', where: 'in /js/summary.js', function: 'renderSummary()' });
 }
 
 function generateAddTaskHTML() {
+    content.innerHTML = '';
+    content.innerHTML =/*html*/`
+        <h1>Add Task</h1>
+    `;
     console.clear();
     console.table({ name: 'generateAddTaskHTML()', where: 'in /js/addTask.js', function: 'renderAddTask()' });
 }
 
 function generateBoardHTML() {
+    content.innerHTML = '';
+    content.innerHTML =/*html*/`
+        <h1>BOARD</h1>
+    `;
     console.clear();
     console.table({ name: 'generateBoardHTML()', where: 'in /js/board.js', function: 'renderBoard()' });
 }
 
 function generateContactsHTML() {
+    content.innerHTML = '';
+    content.innerHTML =/*html*/`
+        <h1>Contacts</h1>
+    `;
     console.clear();
     console.table({ name: 'generateContactsHTML', where: 'in /js/contacts.js', function: 'renderContacts()' });
 }
