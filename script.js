abd477c4d1b0792bdcfd82fc3793a963f964939e
René Porzelt <rene.tube992@gmail.com>
2023-10-22 21:13:08 +0200

    added profile menu

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ async function init() {
     renderBoard(); // Wird noch zu renderSummary() geändert, ist nur zur disign zwecken umgeändert worden :)
     checkWidthInBoard();
 }
+
+function openProfilMenu() {
+    document.getElementById('profile_menu_contain').classList.remove('d-none');
+}
+
+function closeProfilMenu() {
+    document.getElementById('profile_menu_contain').classList.add('d-none');
+}
+
+function stop(event) {
+    event.stopPropagation();
+}
