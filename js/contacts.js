e1303f15a5ee78cb3899f12a19e9bbf3b7252e37
Anthony Hamon <anthony.hamon91@gmail.com>
2023-10-27 17:38:48 +0200

    contact page responsive almost finishced + started with js rendering

--- js/contacts.js
+++ js/contacts.js
@@ -10,6 +10,29 @@ function renderContacts() {
     generateContactsHTML();
 }
 
+function showContactInformation(){
+    let width = window.innerWidth;
+    if (width < 900){
+        showSelectedContactInformations();
+        setCSSMobileContactInformation()
+    }else{
+        showSelectedContactInformations();
+    }
+}
+
+function showSelectedContactInformations(){
+    let contactInformations = document.getElementById('selected-contact-content');
+    contactInformations.innerHTML = returnContactInformations();
+}
+
+function setCSSMobileContactInformation(){
+    let contactList = document.getElementById('contact-list-section');
+    contactList.classList.add('d-none');
+    let contactInformations = document.getElementById('contactInformations');
+    contactInformations.classList.remove('selected_contact_infos');
+    contactInformations.classList.add('mobile_selected_contact_infos');
+}
+
 function contactsBgrColor() {
     document.getElementById('contacts').classList.add('currentTemplate', 'p-none');
     document.getElementById('contacts_mobile').classList.add('currentTemplate', 'p-none');
