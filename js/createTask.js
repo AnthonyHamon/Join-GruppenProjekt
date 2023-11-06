20687218db8788a44151f87206b3de9e17c1d80c
René Porzelt <rene.tube992@gmail.com>
2023-11-06 10:50:02 +0100

    cleaned code in createTask js

--- js/createTask.js
+++ js/createTask.js
@@ -15,135 +15,84 @@ let tasks = [{
     'title': 'asssssssssssssss sssssss sssssssasdadssssss asdadssssssasdadfas fasfsdasdsfsadsad2',
     'description': 'hoch hinauf war der kleine bär der mutvoll um den mond renn nt, doch als die krähe kam, gint er seines weges 2',
     'category': 'user',
-    'status': 'todo'
+    'status': 'to_do'
 }, {
     'id': 3,
     'title': 'The Title 3',
     'description': 'here you see the description 3',
     'category': 'technical',
-    'status': 'inProgress'
+    'status': 'in_progress'
 }];
 
 let currentTask = 'small'
 
-
-
 function renderAllTasks() {
-    let todo = tasks.filter(t => t['status'] == 'todo');
-    let inProgress = tasks.filter(t => t['status'] == 'inProgress');
-    let feedback = tasks.filter(t => t['status'] == 'feedback');
-    let done = tasks.filter(t => t['status'] == 'done');
-
-    renderTasksTodo(todo);
-    renderTasksInProgress(inProgress);
-    renderTasksFeedback(feedback);
-    renderTasksDone(done);
+    renderTasksByStatus('to_do');
+    renderTasksByStatus('in_progress');
+    renderTasksByStatus('feedback');
+    renderTasksByStatus('done');
 }
 
-function renderTasksTodo(todo) {
-    let todoContain = document.getElementById('to_do');
-    todoContain.innerHTML = '';
-    for (i = 0; i < todo.length; i++) {
-        let element = todo[i];
-        todoContain.innerHTML = /*html*/`
-            ${renderTaskHTML(element['id'], element['title'], element['description'], element['category'])}
-        `;
-    }
+function renderTasksByStatus(status) {
+    let container = document.getElementById(status);
+    let tasksByStatus = tasks.filter(task => task.status === status);
+    container.innerHTML = '';
+    tasksByStatus.forEach(task => {
+        container.appendChild(renderTaskElement(task));
+    });
 }
 
-function renderTasksInProgress(inProgress) {
-    let inProgressContain = document.getElementById('in_progress');
-    inProgressContain.innerHTML = '';
-    for (i = 0; i < inProgress.length; i++) {
-        let element = inProgress[i];
-        inProgressContain.innerHTML = /*html*/`
-            ${renderTaskHTML(element['id'], element['title'], element['description'], element['category'])}
-        `;
-    }
+function renderTaskElement(task) {
+    let section = document.createElement('section');
+    section.id = task.id;
+    section.className = 'section';
+    section.innerHTML = renderTaskHTML(task);
+    return section;
 }
 
-function renderTasksFeedback(feedback) {
-    let feedbackContain = document.getElementById('feedback');
-    feedbackContain.innerHTML = '';
-    for (i = 0; i < feedback.length; i++) {
-        let element = feedback[i];
-        feedbackContain.innerHTML = /*html*/`
-            ${renderTaskHTML(element['id'], element['title'], element['description'], element['category'])}
-        `;
-    }
-}
-
-function renderTasksDone(done) {
-    let doneContain = document.getElementById('done');
-    doneContain.innerHTML = '';
-    for (i = 0; i < done.length; i++) {
-        let element = done[i];
-        doneContain.innerHTML = /*html*/`
-            ${renderTaskHTML(element['id'], element['title'], element['description'], element['category'])}
-        `;
-    }
-}
-
-function renderTaskHTML(id, title, description, category) {
+function renderTaskHTML(task) {
     return /*html*/`
-        <section id="${id}" class="section">
-            <div>
-                ${checkTaskCategory(category)}
+        <div>
+            ${checkTaskCategory(task.category)}
+        </div>
+        <article>
+            <span class="taskTitle">${formatTaskText(task.title)}</span>
+            <p class="taskDescription">${formatTaskText(task.description)}</p>
+        </article>
+        <div id="subtask_contain${task.id}" class="subtaskContain">
+            <div class="progressbar-container">
+                <div class="progressbar"></div>
             </div>
-            <article>
-                <span class="taskTitle">${returnTaskTitle(title)}</span>
-                <p class="taskDescription">${returnTaskDescription(description)}</p>
-            </article>
-            <div id="subtask_contain">
-                <div class="progressbar-container">
-                    <div class="progressbar"></div>
-                </div>
-                <span id="subtask">1/2 Subtasks</span>
+            <span id="subtaskTxt${task.id}" class="subtaskTxt">1/2 Subtasks</span>
+        </div>
+        <div class="profilePropertyContain">
+            <div id="profile${task.id}" class="profileContain">
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
+                <div class="profileBadge">RP</div>
             </div>
-            <div class="profilePropertyContain">
-                <div id="profile" class="profileContain">
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                    <div class="profileBadge">RP</div>
-                </div>
-                <img src="/images/Property 1=Low.svg">
-            </div>
-        </section>
+            <img src="/images/Property 1=Low.svg">
+        </div>
     `;
 }
 
-function returnTaskTitle(title) {
-    if (title.length > 57 && currentTask == 'small') {
-        return title.charAt(0).toUpperCase() + title.slice(1).substring(0, 57) + '...';
-    } else if (title.length <= 57 && currentTask == 'small') {
-        return title.charAt(0).toUpperCase() + title.slice(1);
-    } else if (currentTask == 'big') {
-        return title.charAt(0).toUpperCase() + title.slice(1);
+function formatTaskText(text) {
+    if (currentTask === 'small' && text.length > 57) {
+        return text.charAt(0).toUpperCase() + text.slice(1, 57) + '...';
     }
-}
-
-function returnTaskDescription(description) {
-    if (description.length > 57 && currentTask == 'small') {
-        return description.charAt(0).toUpperCase() + description.slice(1).substring(0, 57) + '...';
-    } else if (description.length <= 57 && currentTask == 'small') {
-        return description.charAt(0).toUpperCase() + description.slice(1);
-    } else if (currentTask == 'big') {
-        return description.charAt(0).toUpperCase() + description.slice(1);
-    }
+    return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 function checkTaskCategory(category) {
-    if (category == 'user') {
-        return `<h2 class="userStory">User Story</h2>`;
-    } else {
-        return `<h2 class="technicalTask">Technical Task</h2>`;
-    }
+    return category === 'user' ?
+        `<h2 class="userStory">User Story</h2>` :
+        `<h2 class="technicalTask">Technical Task</h2>`;
 }
 
 
