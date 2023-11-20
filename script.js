864633bcd64b52cf21159cfff496283cb6f082d1
Anthony Hamon <anthony.hamon91@gmail.com>
2023-11-20 16:51:54 +0100

    changed initi with summary instead of board

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function init() {
     await includeHTML();
-    renderBoard(); // Wird noch zu renderSummary() geändert, ist nur zur disign zwecken umgeändert worden :)
+    renderSummary();
 }
 
 function openProfilMenu() {
