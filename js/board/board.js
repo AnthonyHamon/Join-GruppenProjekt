129e14c4c04b30052496795ce0f75403200a1511
René Porzelt <rene.tube992@gmail.com>
2023-11-21 20:44:16 +0100

    change script path in index

--- js/board/board.js
+++ js/board/board.js
@@ -1,8 +1,8 @@
 function renderBoard() {
     generateBoardHTML();
     changeBoardContent();
-    checkWidthInBoard();
     loadingProcess();
+    checkWidthInBoard();
     boardBgrColor();
     removeBgrColorWithoutBoard();
     addJoinLogoClickable();
