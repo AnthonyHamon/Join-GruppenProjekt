fd0c1a2840b321a3dbbcc7b5e0bce3369b97b907
Anthony Hamon <anthony.hamon91@gmail.com>
2024-01-24 16:00:46 +0100

    fixed bug opening edit contact

--- js/returnHTML/returnContactsHTML.js
+++ js/returnHTML/returnContactsHTML.js
@@ -184,7 +184,7 @@ function returnAddContactPopup() {
 
 function returnEditContactPopup(name, email, phone, initial, BgColor) {
     return `
-        <div id="contact-popup" class="contact_popup">
+        <div id="popup" class="contact_popup">
             <section class="section_left">
                 <div class="close_mobile_contact_popup_div">
                     <div onclick="closeMobileContactPopup()" id="close-contact-popup">
